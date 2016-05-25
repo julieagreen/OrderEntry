55f6cf1f6a27fe256ccd613f6393df0f77ead06c
julieagreen <julie.a.green@gmail.com>
2016-05-24 20:27:38 -0700

    first commit

--- OrderEntry/Form1.Designer.cs
+++ OrderEntry/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace OrderEntry
 {
-    partial class Form1
+    partial class FrmMain
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,131 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblItem = new System.Windows.Forms.Label();
+            this.txtItem = new System.Windows.Forms.TextBox();
+            this.lblQty = new System.Windows.Forms.Label();
+            this.txtQty = new System.Windows.Forms.TextBox();
+            this.lblUnitPrice = new System.Windows.Forms.Label();
+            this.txtUnitPrice = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.lblExtendedPrice = new System.Windows.Forms.Label();
+            this.txtExtendedPrice = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblItem
+            // 
+            this.lblItem.AutoSize = true;
+            this.lblItem.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblItem.Location = new System.Drawing.Point(91, 26);
+            this.lblItem.Name = "lblItem";
+            this.lblItem.Size = new System.Drawing.Size(45, 20);
+            this.lblItem.TabIndex = 0;
+            this.lblItem.Text = "Item:";
+            // 
+            // txtItem
+            // 
+            this.txtItem.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtItem.Location = new System.Drawing.Point(157, 24);
+            this.txtItem.Name = "txtItem";
+            this.txtItem.Size = new System.Drawing.Size(154, 24);
+            this.txtItem.TabIndex = 1;
+            // 
+            // lblQty
+            // 
+            this.lblQty.AutoSize = true;
+            this.lblQty.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblQty.Location = new System.Drawing.Point(64, 64);
+            this.lblQty.Name = "lblQty";
+            this.lblQty.Size = new System.Drawing.Size(72, 20);
+            this.lblQty.TabIndex = 2;
+            this.lblQty.Text = "Quantity:";
+            // 
+            // txtQty
+            // 
+            this.txtQty.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtQty.Location = new System.Drawing.Point(157, 64);
+            this.txtQty.Name = "txtQty";
+            this.txtQty.Size = new System.Drawing.Size(61, 24);
+            this.txtQty.TabIndex = 3;
+            // 
+            // lblUnitPrice
+            // 
+            this.lblUnitPrice.AutoSize = true;
+            this.lblUnitPrice.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUnitPrice.Location = new System.Drawing.Point(55, 110);
+            this.lblUnitPrice.Name = "lblUnitPrice";
+            this.lblUnitPrice.Size = new System.Drawing.Size(81, 20);
+            this.lblUnitPrice.TabIndex = 4;
+            this.lblUnitPrice.Text = "Unit Price:";
+            // 
+            // txtUnitPrice
+            // 
+            this.txtUnitPrice.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUnitPrice.Location = new System.Drawing.Point(157, 106);
+            this.txtUnitPrice.Name = "txtUnitPrice";
+            this.txtUnitPrice.Size = new System.Drawing.Size(61, 24);
+            this.txtUnitPrice.TabIndex = 5;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalculate.Location = new System.Drawing.Point(243, 151);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(112, 28);
+            this.btnCalculate.TabIndex = 6;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            // 
+            // lblExtendedPrice
+            // 
+            this.lblExtendedPrice.AutoSize = true;
+            this.lblExtendedPrice.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblExtendedPrice.Location = new System.Drawing.Point(16, 159);
+            this.lblExtendedPrice.Name = "lblExtendedPrice";
+            this.lblExtendedPrice.Size = new System.Drawing.Size(120, 20);
+            this.lblExtendedPrice.TabIndex = 7;
+            this.lblExtendedPrice.Text = "Extended Price:";
+            // 
+            // txtExtendedPrice
+            // 
+            this.txtExtendedPrice.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtExtendedPrice.Location = new System.Drawing.Point(157, 153);
+            this.txtExtendedPrice.Name = "txtExtendedPrice";
+            this.txtExtendedPrice.Size = new System.Drawing.Size(61, 26);
+            this.txtExtendedPrice.TabIndex = 8;
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(367, 215);
+            this.Controls.Add(this.txtExtendedPrice);
+            this.Controls.Add(this.lblExtendedPrice);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtUnitPrice);
+            this.Controls.Add(this.lblUnitPrice);
+            this.Controls.Add(this.txtQty);
+            this.Controls.Add(this.lblQty);
+            this.Controls.Add(this.txtItem);
+            this.Controls.Add(this.lblItem);
+            this.Name = "FrmMain";
+            this.Text = "Order Entry";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblItem;
+        private System.Windows.Forms.TextBox txtItem;
+        private System.Windows.Forms.Label lblQty;
+        private System.Windows.Forms.TextBox txtQty;
+        private System.Windows.Forms.Label lblUnitPrice;
+        private System.Windows.Forms.TextBox txtUnitPrice;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label lblExtendedPrice;
+        private System.Windows.Forms.TextBox txtExtendedPrice;
     }
 }
 
--- OrderEntry/Program.cs
+++ OrderEntry/Program.cs
@@ -16,7 +16,7 @@ namespace OrderEntry
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FrmMain());
         }
     }
 }
